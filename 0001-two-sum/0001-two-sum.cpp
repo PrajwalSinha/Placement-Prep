0364a149e263970a5cb346e41b37927944fbca24
Prajwal sinha <96370170+PrajwalSinha@users.noreply.github.com>
2024-09-04 17:26:35 +0530

    Time: 9 ms (68.05%), Space: 14.5 MB (18.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,26 +1,57 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    //     vector<int> res;
-    //     for (int i = 0; i < nums.size(); i++) {
-    //     for (int j = i + 1; j < nums.size(); j++) {
-    //         if (nums[i] + nums[j] == target){
-    //             res.push_back(i);
-    //             res.push_back(j);
-    //         }
-    //     }
-    // }
-    // return res;
-        map<int,int> mpp;
-        for(int i=0;i<nums.size();i++){
-            int a = nums[i];
-            int more = target - a;
-            if(mpp.find(more)!=mpp.end()){
-                return {mpp[more],i};
-            }
-            mpp[a] = i;
-        }
+        //Brute Force approach
+        // int rem;
+        // vector<int> res;
+        // for(int i=0;i<nums.size();i++){
+        //     rem = target - nums[i];
+        //     for(int j=0;j<nums.size();i++){
+        //         if(rem==nums[j]){
+        //             res.push_back(i);
+        //             res.push_back(j);
+        //         }
+        //     }
+        // }
+        // return res;
         
+        //Better Approach
+//         int rem;
+//         int low;
+//         int high;
+//         vector<int> res;
+//         vector<int> sorted=nums;
+//         sort(sorted.begin(),sorted.end());
+//         for(int i=0;i<sorted.size();i++){
+//             rem = target - sorted[i];
+//             low = i;
+//             high = sorted.size()-1;
+            
+//             while(low<=high){
+//                 int mid = (low+high)/2;
+//                 if(sorted[mid]==rem){
+//                     res.push_back(i);
+//                     res.push_back(mid);
+//                 } else if(sorted[mid]<target){
+//                     low = mid+1;
+//                 } else{
+//                     high = mid-1;
+//                 }
+//             }
+            
+//         }
+//         return res; 
+        
+        //optimal approach
+        map<int,int> mpp;
+        int rem;
+        for(int i=0;i<nums.size();i++){
+            rem = target - nums[i];
+            if(mpp.find(rem)!=mpp.end()){
+                return {mpp[rem],i};
+            }
+            mpp[nums[i]] = i;
+        }
         return {-1,-1};
   }
 };

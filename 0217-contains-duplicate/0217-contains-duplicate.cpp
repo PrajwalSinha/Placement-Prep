5f9d8743fa84d30375f0af7232ca5eeb4affc4d2
Prajwal sinha <96370170+PrajwalSinha@users.noreply.github.com>
2024-09-17 01:25:28 +0530

    Time: 125 ms (15.39%), Space: 74.4 MB (28.79%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,19 +2,32 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         
+//         map<int,int> mpp;
+//         int n = nums.size();
+        
+//         for(int i=0;i<n;i++){
+//             mpp[nums[i]]++;
+//         }
+        
+//         for(int i=0;i<n;i++){
+//             if(mpp[nums[i]]>=2){
+//                 return true;
+//             }
+//         }
+        
+//         return false;
+        
+        //better approach
+        
         map<int,int> mpp;
         int n = nums.size();
         
         for(int i=0;i<n;i++){
-            mpp[nums[i]]++;
-        }
-        
-        for(int i=0;i<n;i++){
-            if(mpp[nums[i]]>=2){
+            if(mpp.find(nums[i])!=mpp.end()){
                 return true;
             }
+            mpp[nums[i]] = i;
         }
-        
         return false;
     }
 };

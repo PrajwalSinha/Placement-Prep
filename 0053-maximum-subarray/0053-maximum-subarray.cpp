078a130fbd51cbbe0a11dc0495ef3ba535664155
Prajwal sinha <96370170+PrajwalSinha@users.noreply.github.com>
2024-09-17 16:40:28 +0530

    Time: 72 ms (77.76%), Space: 70.6 MB (22.57%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,17 +1,41 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-          //kadanes algorithm
-        int sum = 0;
-        int maxi = nums[0];
-        for(int i=0;i<nums.size();i++){
-            sum = sum + nums[i];
-            maxi=max(maxi,sum);
-            if(sum<0){
-                sum=0;
-            }
-        }
+//        map<int,int> mpp;
+//        int sum = 0;
+//        int n = nums.size();
+//        int maxSum = 0;
+//        for(int i=0;i<n;i++){
+//            sum+=nums[i];
+//            int rem = sum - maxSum;
+           
+//            if(mpp.find(rem)!=mpp.end()){
+//                int start = mpp[rem];
+//                int end = i;
+//                int res = 0;
+//                for(int i=start;i<end;i++){
+//                    res+=nums[i];
+//                }
+//                maxSum = max(maxSum,res);
+//            }
+//            mpp[sum] = i;
+//        }
+//         return maxSum+1;
         
-        return maxi;
+        int n = nums.size();
+        int maxSum = INT_MIN;
+        int sum = 0;
+       int pre = 0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum>maxSum){
+                maxSum = sum;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+    
+        }
+        return maxSum;
     }
 };

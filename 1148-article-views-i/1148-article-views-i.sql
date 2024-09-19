fd01ded6c07c893e8a0c28153401bc2c2116c0b3
Mridul Jain <jain.mridul.20@gmail.com>
2024-09-19 10:44:40 +0530

    remove previous format

--- 1148-article-views-i/1148-article-views-i.sql
+++
@@ -1,5 +0,0 @@
-# Write your MySQL query statement below
-select DISTINCT author_id as id
-from Views
-where author_id=viewer_id 
-ORDER BY author_id ASC
--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++
@@ -1,4 +0,0 @@
-# Write your MySQL query statement below
-select customer_id ,count(customer_id) as count_no_trans from Visits WHERE  NOT EXISTS (SELECT visit_id
-                   FROM   Transactions t
-                   WHERE  t.visit_id = Visits.visit_id) group by customer_id
